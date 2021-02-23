8fb4c6a70c31f7e9ded631d934a0f09c5afd9652
sadboy993 <78982039+sadboy993@users.noreply.github.com>
2021-02-23 00:15:30 -0300

    Add files via upload

---
+++ src/vipmenu.js
@@ -0,0 +1,25 @@
+const vipmenu = (prefix) => { 
+	return `            
+	
+╔══✪〘 USUÁRIO VIP 〙✪══
+║
+╠➸ *${prefix}dado*
+╠➸ *${prefix}cekvip*
+╠➸ *${prefix}*
+╠➸ *${prefix}delete*
+╠➸ *${prefix}modapk*
+╠➸ *${prefix}indo10*
+╠➸ *${prefix}*
+╠➸ *${prefix}qrcode*
+╠➥ *${prefix}chentai*
+╠➥ *${prefix}gcpf*
+╠➥ *${prefix}gbin*
+╠➥ *${prefix}pack*
+╠➥ *${prefix}destrava*
+╠➥ *${prefix}gpessoa*
+║
+║
+╚══✪〘  E-GRILO BOT 〙✪══
+`
+}
+exports.vipmenu = vipmenu
